9e51a5c744fee82922a9ed12a305d839e6fcdea2
Jorge Rodríguez García <jorger09@ucm.es>
2017-12-02 17:52:03 +0100

    a ver si ahora se ve, escaleras op

--- PVLI/src/js/load.js
+++ PVLI/src/js/load.js
@@ -3,8 +3,6 @@ var loadScene={
 	//preload es como awake en Unity, es usado para cargar assets normalmente
 	preload:function(){
 		//informamos de que esta cargando
-		/*this.game.load.baseURL = "https://github.com/gonzsa04/DonkeyKong/tree/master/PVLI/src/images";
-		this.game.load.crossOrigin = 'anonymous';*/
 		var loading=game.add.text(180, 250, 'Loading...', {font: '40px Courier', fill: '#FFF'})
 		game.load.image('mario', 'images/mario.png');//sprite de Mario
 		game.load.image('plataforma', 'images/plataforma.png');//sprite de la plataforma
