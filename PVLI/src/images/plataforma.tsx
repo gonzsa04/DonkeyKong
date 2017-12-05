632811f6696bb3fa987c965405e1fdfd7f5fbff9
Jorge Rodríguez García <jorger09@ucm.es>
2017-12-05 13:15:34 +0100

    mapa reescalado y funcional, falta poner en su sitio las escaleras y hacer que mario pueda volver a saltar al caer al suelo (y que si salta y se da en la cabeza con una plataforma pueda atravesarla).

--- PVLI/src/images/plataforma.tsx
+++ PVLI/src/images/plataforma.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="plataforma" tilewidth="2" tileheight="2" tilecount="108" columns="18">
- <image source="plataforma.png" width="36" height="13"/>
+<tileset name="plataforma" tilewidth="2" tileheight="2" tilecount="242" columns="22">
+ <image source="plataforma.png" width="44" height="22"/>
 </tileset>
