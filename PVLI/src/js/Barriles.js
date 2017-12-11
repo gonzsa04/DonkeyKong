743c6f5c7bd26f900a66723c765583ab2cdebbdf
Gonzalo <gonzsa04@ucm.es>
2017-12-11 22:55:59 +0100

    Prueba2 para el hito 2

---
+++ PVLI/src/js/Barriles.js
@@ -0,0 +1,91 @@
+//clase de los barriles, hereda de GameObject
+class Barril extends GameObject{
+
+    //-----------------------------------CONSTRUCTORA--------------------------------------------
+    //constructora de los barriles
+    constructor(x, y, nombre){
+        super(x, y, nombre);//llama a constructora de GameObject
+        this._velX = 20;//velocidades a las que cae de una plataforma a otra
+        this._velY = 80;
+        this._baja=-1;//variable que elige si baja o no por una escalera
+        this._cambia = false;//indica si debe cambiar de velocidad
+        this._rotaAnim = false;
+        this._escaleraAnim = false;
+        //redimensionamos su collider
+        this._gameObject.body.setSize(this._gameObject.width*4/5, this._gameObject.height*3/5);
+
+        //ANIMACIONES
+        this._anim = this._gameObject.animations;//todas se guardaran en anim
+        this._anim.add('rotate', [0,1,2,3], 8, true);//rotar
+        this._anim.add('escalera', [4,5], 6, true);//bajar escaleras
+        this._anim.play('rotate');
+    }
+    //--------------------------------------------------------------------------------------------
+
+
+    //--------------------------------------MOVIMIENTO--------------------------------------------
+    //mueve al barril
+    muevete(){
+        this._gameObject.body.velocity.x=this._vel;
+        if(this._vel < 0) this._gameObject.scale.setTo(-1, 1);
+        else this._gameObject.scale.setTo(1, 1);
+    }
+    //--------------------------------------------------------------------------------------------
+
+
+    //--------------------------------------UPDATE------------------------------------------------
+    //update del barril, hace que choque con el suelo
+    update(plataformas){
+        if(!this._atraviesa) {
+            game.physics.arcade.collide(this._gameObject, plataformas);
+            this.muevete();//lo mueve
+        }
+        //si ha caido de una plataforma a otra
+        if(this._gameObject.body.velocity.y >= this._velY){
+            this._cambia=true;//puede cambiar de direccion
+            this._gameObject.body.velocity.y = this._velY;//hacemos que caiga en parabola
+            if (this._gameObject.body.velocity.x != 0) this._gameObject.body.velocity.x = this._velX*(this._vel/Math.abs(this._vel));
+        }
+        //si toca el suelo
+        if(this._gameObject.body.onFloor()){
+            if (this._cambia){//si puede cambiar de direccion lo hace
+                this._atraviesa=false;
+                this._cambia=false;
+                this._vel=-this._vel;
+                this._escaleraAnim = false;
+                if(!this._rotaAnim){ //Si puede rotar y no esta en una escalera
+                    this._anim.play("rotate");//se pone la animacion de rotar
+                    this._rotaAnim = true;
+                }
+            }
+        }
+    }
+    //---------------------------------------------------------------------------------------------
+
+
+    //------------------------------------------AUXILIARES-----------------------------------------
+    //decide si baja por una escalera o no
+    bajaOno(){
+        //si aun no se ha decidido, se elige si baja o no (cuando deje de colisionar con una escalera se volvera
+        //a marcar como no decidido para futuras escaleras)
+        if(this._baja == -1){
+            this._baja = Math.floor(Math.random()*2);
+            if(this._baja == 1){
+                this._atraviesa=true;
+                this._gameObject.body.velocity.x=0;
+                if(!this._escaleraAnim){//si puede bajar y esta en una escalera
+                    this._anim.play("escalera");//se pone la animacion de escalera
+                    this._escaleraAnim = true;
+                    this._rotaAnim = false;
+                }
+            }
+        }
+    }
+
+    //deja de atravesar muros
+    noAtravieses(){ this._atraviesa = false; }
+
+    //marca a la variable que decide si se baja o no como no decidido
+    noDecidido(){ this._baja = -1; }
+    //----------------------------------------------------------------------------------------------
+}
---
+++ PVLI/src/js/GameObject.js
@@ -0,0 +1,31 @@
+//clase GameObject para las entidades del juego
+class GameObject{
+    //constructora de la clase
+    constructor(x, y, nombre, frame){
+        this._gameObject=game.add.sprite(x, y, nombre, frame);//carga el sprite
+        this._vel = 125;//velocidad a la que se mueve
+        this._atraviesa = false;//indica si puede atravesar muros
+        game.physics.arcade.enable(this._gameObject);//habilitamos fisicas, gravedad, etc.
+        this._gameObject.body.gravity.y=400;
+        this._gameObject.body.setSize(this._gameObject.width, this._gameObject.height/5);//collider, solo en sus pies
+        this._gameObject.anchor.setTo(0.5, 1);//establecemos su centro en sus pies
+        this._gameObject.checkWorldBounds = true;//si se sale de los bordes de la pantalla muere
+        this._gameObject.events.onOutOfBounds.add(this.morir, this);
+    }
+
+    //cuando muere se destruye
+    morir(){ this._gameObject.kill(); }
+
+    //devuelven la posicion del gameObject
+    get x(){ return this._gameObject.x; }
+    get y(){ return this._gameObject.y; }
+
+    //devuelven el gameObject
+    get gameObject(){ return this._gameObject; }
+    
+    //devuelve si esta vivo
+    estaVivo(){ return this._gameObject.alive; }
+    
+    //lo spawnea en la posicion x, y
+    spawn(x, y){ this._gameObject.reset(x, y); }
+}
